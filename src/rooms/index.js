b2fcc4308aa8d6a09b6982847dad849425d8560a
mattrossman <rossman@gatech.edu>
2021-04-07 00:48:14 -0400

    Add parallax component and blender config

--- src/rooms/index.js
+++ src/rooms/index.js
@@ -1,6 +1,8 @@
 import '../systems/fader-plus.js'
 import '../components/portal.js'
 import '../components/immersive-360.js'
+import '../components/parallax.js'
 
 AFRAME.GLTFModelPlus.registerComponent('immersive-360', 'immersive-360')
 AFRAME.GLTFModelPlus.registerComponent('portal', 'portal')
+AFRAME.GLTFModelPlus.registerComponent('parallax', 'parallax')
