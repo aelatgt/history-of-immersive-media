8c9bb018bd119d833e148868363165945b845d9d
mattrossman <rossman@gatech.edu>
2021-04-15 12:00:41 -0400

    Refactor immersive-360 to work with GIF textures

--- src/components/immersive-360.js
+++ src/components/immersive-360.js
@@ -19,35 +19,61 @@ AFRAME.registerComponent('immersive-360', {
   schema: {
     url: { type: 'string', default: null },
   },
-  init: function () {
+  init: async function () {
     const url = this.data.url ?? this.parseSpokeName()
-    this.el.setAttribute('geometry', { primitive: 'sphere', radius: 300 })
+    const extension = url.match(/^.*\.(.*)$/)[1]
 
-    this.near = 1
-    this.far = 1.3
-    this.el.setAttribute('material', {
+    // media-image will set up the sphere geometry for us
+    this.el.setAttribute('media-image', {
+      projection: '360-equirectangular',
+      alphaMode: 'opaque',
       src: url,
-      shader: 'flat',
-      side: 'back',
+      version: 1,
+      batch: false,
+      contentType: `image/${extension}`,
+      alphaCutoff: 0,
+    })
+    // but we need to wait for this to happen
+    this.mesh = await this.getMesh()
+    this.mesh.geometry.scale(100, 100, 100)
+    this.mesh.material.setValues({
       transparent: true,
       depthTest: false,
     })
+    this.near = 1
+    this.far = 1.3
+
     // Render OVER the scene but UNDER the cursor
-    this.el.object3DMap.mesh.renderOrder = APP.RENDER_ORDER.CURSOR - 1
+    this.mesh.renderOrder = APP.RENDER_ORDER.CURSOR - 1
   },
   tick: function () {
-    // Linearly map camera distance to material opacity
-    this.el.object3D.getWorldPosition(worldSelf)
-    this.el.sceneEl.camera.getWorldPosition(worldCamera)
-    const distance = worldSelf.distanceTo(worldCamera)
-    const opacity = 1 - (distance - this.near) / (this.far - this.near)
-    this.el.components.material.material.opacity = opacity
+    if (this.mesh) {
+      // Linearly map camera distance to material opacity
+      this.mesh.getWorldPosition(worldSelf)
+      this.el.sceneEl.camera.getWorldPosition(worldCamera)
+      const distance = worldSelf.distanceTo(worldCamera)
+      const opacity = 1 - (distance - this.near) / (this.far - this.near)
+      this.mesh.material.opacity = opacity
+    }
   },
   parseSpokeName: function () {
     // Accepted names: "label__image-hash_ext" OR "image-hash_ext"
     const spokeName = this.el.parentEl.parentEl.className
-    const [, hash, extension] = spokeName.match(/(?:.*__)?(.*)_(.*)/)[1]
+    const [, hash, extension] = spokeName.match(/(?:.*__)?(.*)_(.*)/)
     const url = `https://gt-ael-aq-assets.aelatgt-internal.net/files/${hash}.${extension}`
     return url
+  },
+  getMesh: async function () {
+    return new Promise((resolve) => {
+      const mesh = this.el.object3DMap.mesh
+      if (mesh) resolve(mesh)
+      this.el.addEventListener(
+        'image-loaded',
+        () => {
+          resolve(this.el.object3DMap.mesh)
+        },
+        { once: true }
+      )
+    })
   },
 })
