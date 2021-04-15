641d4af85f0ba2cb99df6f402ae998646acde289
Matt Rossman <rossman@gatech.edu>
2021-04-15 11:10:31 -0400

    Add parallax component and blender config (#5)

---
+++ src/components/parallax.js
@@ -0,0 +1,73 @@
+/**
+ * Description
+ * ===========
+ * Create the illusion of depth in a color image from a depth map
+ *
+ * Usage
+ * =====
+ * Create a plane in Blender and give it a material (just the default Principled BSDF).
+ * Assign color image to "color" channel and depth map to "emissive" channel.
+ * You may want to set emissive strength to zero so the preview looks better.
+ * Add the "parallax" component from the Hubs extension, configure, and export as .glb
+ */
+
+import { ParallaxShader } from '../shaders/parallax-shader.js'
+
+const vec = new THREE.Vector3()
+const forward = new THREE.Vector3(0, 0, 1)
+
+AFRAME.registerComponent('parallax', {
+  schema: {
+    strength: { type: 'number', default: 0.5 },
+    cutoffTransition: { type: 'number', default: Math.PI / 8 },
+    cutoffAngle: { type: 'number', default: Math.PI / 4 },
+  },
+  init: function () {
+    const mesh = this.el.object3DMap.mesh
+    const { map: colorMap, emissiveMap: depthMap } = mesh.material
+    colorMap.wrapS = colorMap.wrapT = THREE.ClampToEdgeWrapping
+    depthMap.wrapS = depthMap.wrapT = THREE.ClampToEdgeWrapping
+    const { vertexShader, fragmentShader } = ParallaxShader
+    this.material = new THREE.ShaderMaterial({
+      vertexShader,
+      fragmentShader,
+      defines: { USE_OCLUSION_PARALLAX: true },
+      uniforms: {
+        map: { value: colorMap },
+        bumpMap: { value: depthMap },
+        parallaxScale: { value: -1 * this.data.strength },
+        parallaxMinLayers: { value: 20 },
+        parallaxMaxLayers: { value: 30 },
+        fade: { value: 0 },
+      },
+    })
+    mesh.material = this.material
+  },
+  tick() {
+    if (this.el.sceneEl.camera) {
+      this.el.sceneEl.camera.getWorldPosition(vec)
+      this.el.object3D.worldToLocal(vec)
+      const angle = vec.angleTo(forward)
+      const fade = mapLinearClamped(
+        angle,
+        this.data.cutoffAngle - this.data.cutoffTransition,
+        this.data.cutoffAngle + this.data.cutoffTransition,
+        0, // In view zone, no fade
+        1 // Outside view zone, full fade
+      )
+      this.material.uniforms.fade.value = fade
+    }
+  },
+})
+
+function clamp(value, min, max) {
+  return Math.max(min, Math.min(max, value))
+}
+
+function mapLinear(x, a1, a2, b1, b2) {
+  return b1 + ((x - a1) * (b2 - b1)) / (a2 - a1)
+}
+
+function mapLinearClamped(x, a1, a2, b1, b2) {
+  return clamp(mapLinear(x, a1, a2, b1, b2), b1, b2)
+}
